58b0611a551fd51d89eb22fa41ae18bd95558592
Brainlulz <alex71labonne@gmail.com>
2017-10-31 02:53:14 +0100

    modif

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
 const app = angular.module('app', []);
+app.config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'https://api.lookr.com/embed/player/**'
+  ]);
+});
--- public/js/components/webcamComponent.js
+++ public/js/components/webcamComponent.js
@@ -4,10 +4,12 @@ angular.module('app')
 
     templateUrl: 'js/components/webcamComponent.html',
 
-    controller: function (WebcamsService, $log, $sce, $scope) {
+    controller: function (WebcamsService, $log, $sce) {
       this.$onInit = ($scope, $sce) => {
         $log.info('Webcam component initialized');
-       
+        this.getIframeSrc = function(src) {
+          return 'https://api.lookr.com/embed/player/' +src +'/day?autoresize=1&amp;referrer=http%3A%2F%2Flocalhost%3A3000%2F';
+        };
         WebcamsService.getWebcam().then((webcams) => {
           this.webcam = webcams;
           
