4588513885ecf346dea0f40f728048b17036b6a0
m1roh <hayromain.rh@gmail.com>
2017-11-01 09:03:13 +0100

    Deleted unused directory

--- server/routes/weather.js
+++
@@ -1,8 +0,0 @@
-const WeatherController = require('../controllers/weather');
-
-module.exports = (router) => {
-
-  let controller = new WeatherController();
-
-  router.get('/', (req, res) => controller.getWeather(req, res));
-};
--- server/routes/webcams.js
+++
@@ -1,8 +0,0 @@
-const WebcamsController = require('../controllers/weather');
-
-module.exports = (router) => {
-
-  let controller = new WebcamsController();
-
-  router.get('/', (req, res) => controller.getCity(req, res));
-};
